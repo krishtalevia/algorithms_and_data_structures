0d3336247139ad8964e7cbf0bb092df8f277fff7
krishtalevia <krishtalevia@gmail.com>
2024-06-24 16:00:44 +0300

    fix task2

--- main_4.1.py
+++ main_4.1.py
@@ -1,14 +1,11 @@
 import math
 
 def input_number(number: str) -> float:
-    number = input(number)
-
     while True:
         try:
-            number = float(number)
-            return number
+            return float(input(number))
         except ValueError:
-            print('Неподходящий тип данных!')
+            print('Неподходящий тип данных! Введите снова.')
             continue
 
 # 1 Квадратное уравнение
@@ -19,17 +16,19 @@ def quadratic_equation():
     b = input_number('b = ')
     c = input_number('c = ')
 
+    if (a == 0) or (b == 0):
+        print('Коэффициенты равны нулю!')
+        return
+
     discriminant = b ** 2 - 4 * a * c
 
     if discriminant > 0:
         root_1 = (-b + math.sqrt(discriminant)) / (2 * a)
         root_2 = (-b - math.sqrt(discriminant)) / (2 * a)
-
         print('Корни уравнения: ', root_1, 'и', root_2)
 
     elif discriminant == 0:
-        root = -b / 2 * a
-
+        root = -b / (2 * a)
         print('Единственный корень уравнения: ', root)
 
     else:
@@ -38,12 +37,14 @@ def quadratic_equation():
 # 2 (Площадь треугольника)
 
 def triangle_area():
-    print ('Введите длины сторон треугольника a, b и c')
+    print('Введите длины сторон треугольника a, b и c')
     a = input_number('a = ')
     b = input_number('b = ')
     c = input_number('c = ')
 
-    if (c <= a + b) or (b <= a + c) or (a <= b + c):
+    if (a + b > c) or (a + c > b) or (b + c > a):
         p = (a + b + c) / 2
         area = math.sqrt(p * (p - a) * (p - b) * (p - c))
         print('Площадь треугольника: ', area)
+    else:
+        print('Треугольник с такими сторонами не может существовать!')
