82e8908f5f351901a53dc9d1616759f6d5732cbf
krishtalevia <krishtalevia@gmail.com>
2024-06-24 15:51:07 +0300

    feat: input number

--- main_4.1.py
+++ main_4.1.py
@@ -1,37 +1,49 @@
 import math
 
-# 1
+def input_number(number: str) -> float:
+    number = input(number)
 
-print('Введите коэффициенты a, b и c для уравнения ax^2 + bx + c = 0')
-a = float(input('a = '))
-b = float(input('b = '))
-c = float(input('c = '))
+    while True:
+        try:
+            number = float(number)
+            return number
+        except ValueError:
+            print('Неподходящий тип данных!')
+            continue
 
-discriminant = b ** 2 - 4 * a * c
+# 1 Квадратное уравнение
 
-if discriminant > 0:
-    root_1 = (-b + math.sqrt(discriminant)) / (2 * a)
-    root_2 = (-b - math.sqrt(discriminant)) / (2 * a)
+def quadratic_equation():
+    print('Введите коэффициенты a, b и c для уравнения ax^2 + bx + c = 0')
+    a = input_number('a = ')
+    b = input_number('b = ')
+    c = input_number('c = ')
 
-    print('Корни уравнения: ', root_1, 'и', root_2)
+    discriminant = b ** 2 - 4 * a * c
 
-elif discriminant == 0:
-    root = -b / 2 * a
+    if discriminant > 0:
+        root_1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        root_2 = (-b - math.sqrt(discriminant)) / (2 * a)
 
-    print('Единственный корень уравнения: ', root)
+        print('Корни уравнения: ', root_1, 'и', root_2)
 
-else:
-    print('У уравнения нет действительных корней')
+    elif discriminant == 0:
+        root = -b / 2 * a
+
+        print('Единственный корень уравнения: ', root)
+
+    else:
+        print('У уравнения нет действительных корней')
 
 # 2 (Площадь треугольника)
 
-print ('Введите длины сторон треугольника a, b и c')
-a = float(input('a = '))
-b = float(input('b = '))
-c = float(input('c = '))
+def triangle_area():
+    print ('Введите длины сторон треугольника a, b и c')
+    a = input_number('a = ')
+    b = input_number('b = ')
+    c = input_number('c = ')
 
-if (c <= a + b) or (b <= a + c) or (a <= b + c):
-    p = (a + b + c) / 2
-    area = math.sqrt(p * (p - a) * (p - b) * (p - c))
-    print('Площадь треугольника: ', area)
-    
+    if (c <= a + b) or (b <= a + c) or (a <= b + c):
+        p = (a + b + c) / 2
+        area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+        print('Площадь треугольника: ', area)
