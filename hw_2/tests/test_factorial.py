c7e5849feda43e7998d601a5b36ac982bfd052b4
krishtalevia <krishtalevia@gmail.com>
2024-06-30 12:24:32 +0300

    add 10 tests to factorial

--- hw_2/tests/test_factorial.py
+++ hw_2/tests/test_factorial.py
@@ -1,12 +1,21 @@
 import pytest
 from hw_2.src.factorial import factorial
+import math
 
 
 @pytest.mark.parametrize('value, expected_result',
                          [
+                             (1, 1),
+                             (2, 2),
+                             (3, 6),
+                             (4, 24),
                              (5, 120),
+                             (10, 3628800),
+                             (15, 1307674368000),
+                             (20, math.factorial(20)),
+                             (50, math.factorial(50)),
                              (0, 1),
-                             (100, 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000),
+                             (100, math.factorial(100)),
                              (5, 120)
                          ])
 
@@ -17,10 +26,12 @@ def test_factorial_positive(value, expected_result):
                          [
                              (5.0, pytest.raises(TypeError)),
                              (-5, pytest.raises(ValueError)),
+                             (-10, pytest.raises(ValueError)),
                              (-5.0, pytest.raises(ValueError)),
                              (None, pytest.raises(TypeError)),
                              (True, pytest.raises(AssertionError)),
-                             ([5], pytest.raises(TypeError))
+                             ([5], pytest.raises(TypeError)),
+                             (5.0, pytest.raises(TypeError))
                          ])
 
 def test_average_negative(value, expected_result):
