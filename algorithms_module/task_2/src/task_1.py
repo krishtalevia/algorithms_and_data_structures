f0e142a8be1080e9c6dd3f95dcc99b5c5d331415
krishtalevia <krishtalev.ilya@gmail.com>
2024-07-11 18:47:24 +0300

    add task 1 + factorial

---
+++ algorithms_module/task_2/src/task_1.py
@@ -0,0 +1,14 @@
+def factorial(n: int) -> int:
+    is_n_valid(n)
+
+    result = 1
+
+    for i in range(1, n+1, 1):
+        result *= i
+
+    return result
+
+def is_n_valid(n: int) -> int:
+    if not isinstance(n, int): raise TypeError()
+
+    if n <= 0 or n >= 20: raise ValueError()
