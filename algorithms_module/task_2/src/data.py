2636437d48ca0183c4efa2da0e75b7f230742460
krishtalevia <krishtalev.ilya@gmail.com>
2024-07-12 13:12:40 +0300

    feat: max month, max day

--- algorithms_module/task_2/src/data.py
+++ algorithms_module/task_2/src/data.py
@@ -1,40 +1,93 @@
-with open('data.txt', 'r') as file:
-    base_data = file.read()
+def read_data(file_name: str) -> list:
+    with open('data.txt', 'r') as file:
+        base_data = file.read()
 
-data = []
-base_data = base_data.split('\n')
-for i in base_data:
-    temp = i.split(',')
-    data.append(temp)
+    data = []
+    base_data = base_data.split('\n')
+    for i in base_data:
+        temp = i.split(',')
+        data.append(temp)
 
-days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
-count = 0
+    return data
 
-for i in data:
-    i.append(days[count])
-    count += 1
+def expend_data_with_days(data: list) -> list:
+    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    count = 0
 
-    if count == 6:
-        count = 0
+    for i in data:
+        i.append(days[count])
+        count += 1
 
-months_traffic = {'01': ['Январь', 0], '02': ['Февраль', 0], '03': ['Март', 0],
-          '04': ['Апрель', 0], '05': ['Май', 0], '06': ['Июнь', 0],
-          '07': ['Июль', 0], '08': ['Август', 0], '09': ['Сентябрь', 0],
-          '10': ['Октябрь', 0], '11': ['Ноябрь', 0], '12': ['Декабрь', 0]}
+        if count == 6:
+            count = 0
 
-sum = 0
-current_month = data[0][0][5:7]
+    return data
 
-for i in data:
-   month = i[0][5:7]
+def max_month(data: list) -> str:
 
-   if month != current_month:
-       months_traffic[current_month][1] += sum
-       sum = 0
-       current_month = month
+    months_traffic = {'01': ['Январь', 0], '02': ['Февраль', 0], '03': ['Март', 0],
+              '04': ['Апрель', 0], '05': ['Май', 0], '06': ['Июнь', 0],
+              '07': ['Июль', 0], '08': ['Август', 0], '09': ['Сентябрь', 0],
+              '10': ['Октябрь', 0], '11': ['Ноябрь', 0], '12': ['Декабрь', 0]}
 
-   sum += int(i[1])
+    sum = 0
+    current_month = data[0][0][5:7]
 
-months_traffic[current_month][1] += sum
+    for i in data:
+       month = i[0][5:7]
 
-print(months_traffic)
+       if month != current_month:
+           months_traffic[current_month][1] += sum
+           sum = 0
+           current_month = month
+
+       sum += int(i[1])
+
+    months_traffic[current_month][1] += sum
+
+    max_month = 'Январь'
+    max_traffic = 0
+    for i in months_traffic:
+        traffic = months_traffic[i][1]
+        if traffic > max_traffic:
+            max_traffic = traffic
+            max_month = months_traffic[i][0]
+
+    return max_month
+
+def max_day(data: list) -> str:
+    days_traffic = {'Понедельник': 0, 'Вторник': 0, 'Среда': 0,
+                    'Четверг': 0, 'Пятница': 0, 'Суббота': 0,
+                    'Воскресенье': 0}
+
+    sum = 0
+    current_day = data[0][2]
+
+    for i in data:
+       day = i[2]
+
+       if day != current_day:
+           days_traffic[current_day] += sum
+           sum = 0
+           current_day = day
+
+       sum += int(i[1])
+
+    days_traffic[current_day] += sum
+
+    max_day = 'Воскресенье'
+    max_traffic = 0
+    for i in days_traffic:
+        traffic = days_traffic[i]
+        if traffic > max_traffic:
+            max_traffic = traffic
+            max_day = i
+
+    return max_day
+
+def main():
+    data = read_data('data.txt')
+    data = expend_data_with_days(data)
+    print(max_day(data))
+
+main()
