8212165b81329fb8a5fd73360087c8eeea776e10
krishtalevia <krishtalev.ilya@gmail.com>
2024-07-16 12:42:06 +0300

    fix: max in range

--- algorithms_module/task_3/src/max_in_range.py
+++ algorithms_module/task_3/src/max_in_range.py
@@ -1,22 +1,24 @@
-def max_in_range(list: list[int or float], start: int, end: int) -> list:
-    is_request_valid(list, start, end)
+def max_in_range(lst: list[int or float], start: int, end: int) -> list:
+    is_request_valid(lst, start, end)
 
-    end += -len(list)
-    max = list[start]
-    absolute_index = 0
+    max_value = lst[start]
+    absolute_index = start
     relative_index = 0
-    for i in range(start, len(list) + end, 1):
-        if list[i] > max:
-            max = list[i]
+
+    for i in range(start, end + 1, 1):
+        if lst[i] > max:
+            max_value = lst[i]
             absolute_index = i
             relative_index = i - start
 
-    return [max, absolute_index, relative_index]
+    return [max_value, absolute_index, relative_index]
 
-def is_request_valid(list: list[int or float], start, end) -> None:
-    for i in list:
+def is_request_valid(lst: list[int or float], start, end) -> None:
+    if not isinstance(lst, list): raise TypeError()
+
+    for i in lst:
         if not isinstance(i, int or float): raise TypeError()
 
-    if start < 0 or start > len(list): raise ValueError()
-    if end < 0 or end > len(list): raise ValueError()
+    if start < 0 or start > len(lst): raise ValueError()
+    if end < 0 or end > len(lst): raise ValueError()
     if start > end: raise ValueError()
