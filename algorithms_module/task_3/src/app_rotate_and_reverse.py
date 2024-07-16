62c70c12802f93c90c061ef29975a772d7a7c189
krishtalevia <krishtalev.ilya@gmail.com>
2024-07-16 15:01:56 +0300

    fix app reverse even elements, add app rotate and reverse

--- algorithms_module/task_3/src/app_rotate_and_reverse.py
+++ algorithms_module/task_3/src/app_rotate_and_reverse.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from reverse_even_elements import *
+from rotate_and_reverse import *
 
 class App:
 
@@ -8,6 +8,7 @@ class App:
         self.__root_window = root
         self.user_input_ui = None
         self.output_result = None
+        self.k_num_ui = None
         self.__config_window()
         self.__build_main_screen()
 
@@ -15,7 +16,7 @@ class App:
         self.__root_window.mainloop()
 
     def __config_window(self):
-        self.__root_window.title('Reverse even elements')
+        self.__root_window.title('Rotate and reverse')
         self.__root_window.geometry('800x400')
         self.__root_window.resizable(False, False)
 
@@ -32,8 +33,18 @@ class App:
         self.user_input_ui = ttk.Entry(master=main_frame, width=40)
         self.user_input_ui.grid(row=0, column=1, pady=10)
 
+        k_num_label = ttk.Label(
+            master=main_frame,
+            text='Кол-во сдвигов:',
+            font=('Arial', 14)
+        )
+        k_num_label.grid(row=1, column=0, pady=10)
+
+        self.k_num_ui = ttk.Entry(master=main_frame, width=20)
+        self.k_num_ui.grid(row=1, column=1, pady=10)
+
         solver_button = ttk.Button(master=main_frame, text='Решить')
-        solver_button.grid(row=1, columnspan=2, pady=10)
+        solver_button.grid(row=3, columnspan=2)
 
         solver_button.config(command=self.on_click_solver_button_handler)
 
@@ -51,17 +62,20 @@ class App:
 
     def on_click_solver_button_handler(self):
         list_text = self.user_input_ui.get()
+        k_num_text = self.k_num_ui.get()
 
         try:
-            if not list_text:
-                self.output_result.config(text='Заполните поле! (Введите цифры через запятую)')
+            if not list_text or not k_num_text:
+                self.output_result.config(text='Все поля должны быть заполнены!')
                 return
 
             lst = list_text.split(',')
             for i in range(0, len(lst), 1):
                 lst[i] = int(lst[i])
 
-            result = reverse_even_elements(lst)
+            k = int(k_num_text)
+
+            result = rotate_and_reverse(lst, k)
             self.output_result.config(text=f'{result}')
 
         except Exception:
