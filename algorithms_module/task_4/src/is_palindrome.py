a8924d71225b6f02459e0cabd542eb75b5ce39e3
krishtalevia <krishtalev.ilya@gmail.com>
2024-07-21 17:13:18 +0300

    add is palindrome func

---
+++ algorithms_module/task_4/src/is_palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(text) -> bool:
+    assert isinstance(text, str), TypeError()
+
+    if len(text) <= 1:
+        return True
+
+    if text[0] != text[-1]:
+        return False
+
+    return is_palindrome(text[1:-1])
